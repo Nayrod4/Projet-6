90bffcea7d55e198882df55fe7d86d92f50828e6
Nayrod4 <lightfusion4444@gmail.com>
2021-06-16 10:21:06 +0200

    Merge branch 'main' of https://github.com/Nayrod4/Projet-6 into main

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const path = require('path');
+const helmet = require('helmet');
+
+mongoose.connect(process.env.MONGO_URI, {  useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const sauceRoutes = require('./routes/sauce')
+const userRoutes = require('./routes/user');   
+const app = express();
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
+app.use(helmet());
+
+module.exports = app;
