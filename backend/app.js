aae8b955c52fa3e597aec2e57ad647b548c7fa86
Nayrod4 <lightfusion4444@gmail.com>
2021-07-06 15:42:20 +0200

    Maj multer

--- backend/app.js
+++ backend/app.js
@@ -35,6 +35,7 @@ app.use((req, res, next) => {
     }
   }));
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
